0df56eaa5d0e6d4fc7c5694ef5cbf2b8985c4ca7
cleberdevs <129439195+cleberdevs@users.noreply.github.com>
2024-04-18 17:38:22 +0000

    criado arquivo processamento

---
+++ processamento.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import requests
+from io import StringIO 
+import json
+
+class Processamento:
+    @staticmethod
+    def baixar_e_processar_csv(url):
+        resposta = requests.get(url)
+        resposta.encoding = 'utf-8' 
+        resposta.raise_for_status()  # Lança um erro para respostas não bem-sucedidas
+        #dados = pd.read_csv(StringIO(resposta.text))  # Modifique esta linha
+        dados = pd.read_csv(StringIO(resposta.text), sep="\t")
+        dados.to_clipboard()
+       
+        resultado_final_= dados.to_json(orient="records")
+        return json.loads(resultado_final_)
