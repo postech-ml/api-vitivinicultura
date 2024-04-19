43bcb1247495826c7eda2f8d233ada01578088c1
cleberdevs <129439195+cleberdevs@users.noreply.github.com>
2024-04-19 22:58:36 +0000

    alteração app

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from token_generator import TokenGenerator
+from rotas_processamento import RotasProcessamento
 
 app = Flask(__name__)
 
+# Chamar o método criar_token da classe TokenGenerator
+TokenGenerator.criar_token(app)
+
+# Registrar as rotas de processamento
+RotasProcessamento.registrar_rotas(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
