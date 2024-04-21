7a0e89f7b4724d52fcd2e259faec08be3d2b7e34
Cleber de Assis Ferreira <cleberdevs@gmail.com>
2024-04-21 16:32:16 -0300

    alteração teste

--- rotas_processamento.py
+++ rotas_processamento.py
@@ -5,12 +5,12 @@ from processamento import Processamento
 class RotasProcessamento:
     @staticmethod
     def registrar_rotas(app):
-        @app.route('/processamesa', methods=['GET'])
-        @jwt_required()
-        def processa_mesa():
-            url = "http://vitibrasil.cnpuv.embrapa.br/download/ProcessaMesa.csv"
-            dados_json = Processamento.baixar_e_processar_csv(url)
-            return jsonify(dados_json)
+    #     @app.route('/processamesa', methods=['GET'])
+    #     @jwt_required()
+    #     def processa_mesa():
+    #         url = "http://vitibrasil.cnpuv.embrapa.br/download/ProcessaMesa.csv"
+    #         dados_json = Processamento.baixar_e_processar_csv(url)
+    #         return jsonify(dados_json)
 
         @app.route('/processavinifera')
         @jwt_required()
