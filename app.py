07596ce32f96f4156aa9358cd6614cfe93fdc3a8
cleberdevs <129439195+cleberdevs@users.noreply.github.com>
2024-04-21 20:31:53 +0000

    Criação swagger

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from swagger import Swagger
 from token_generator import TokenGenerator
 from rotas_processamento import RotasProcessamento
+
 
 app = Flask(__name__)
 
@@ -12,3 +14,5 @@ RotasProcessamento.registrar_rotas(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
+    Swagger.registrar_rotas(Flask(__name__))
+    Swagger.run()
---
+++ swagger.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_restplus import Api
+from rotas_processamento import RotasProcessamento
+
+class Swagger:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.api = Api(self.app, version='1.0', title='API de Exemplo', description='Descrição da API')
+
+        # Registrar as rotas do arquivo rotas_processamento.py
+        RotasProcessamento.registrar_rotas(self.app)
+
+ 
