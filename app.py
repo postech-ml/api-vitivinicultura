9c0c5e636548e5f51cfb07158b6d6704f3acee98
cleberdevs <129439195+cleberdevs@users.noreply.github.com>
2024-04-22 05:37:33 +0000

    update swagger_config

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask
-from swagger import Swagger
+from flask_restx import Api
+from swagger_config import SwaggerConfig
 from token_generator import TokenGenerator
-from rotas_processamento import RotasProcessamento
-from werkzeug.utils import cached_property
+from rotas_processamento import api as processamento_api
 
 app = Flask(__name__)
-
+api = Api(app)
+SwaggerConfig.configurar(api)
+# Swagger(app)
 # Chamar o método criar_token da classe TokenGenerator
-TokenGenerator.criar_token(app)
+TokenGenerator.criar_token(app) 
 
 # Registrar as rotas de processamento
-RotasProcessamento.registrar_rotas(app)
+# RotasProcessamento.registrar_rotas(app)
+api.add_namespace(processamento_api) 
 
 if __name__ == "__main__":
     app.run(debug=True)
-    Swagger.registrar_rotas(Flask(__name__))
-    Swagger.run()
+   
