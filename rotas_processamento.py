b594af523bd485a9a97a7429c24863b657fe0845
cleberdevs <129439195+cleberdevs@users.noreply.github.com>
2024-04-22 23:04:17 +0000

    update rotas processamento

--- rotas_processamento.py
+++ rotas_processamento.py
@@ -58,9 +58,29 @@ class ProcessaVinifera(Resource):
         url = "http://vitibrasil.cnpuv.embrapa.br/download/ProcessaViniferas.csv"
         dados_json = Processamento.baixar_e_processar_csv(url)
         return dados_json, 200
+    
+class ProcessaSemClass(Resource):
+    @api.doc('processa_sem_class')
+    @api.response(200, 'Sucesso')
+    @jwt_required()
+    def get(self):
+        """Processa dados de sem classificação"""
+        url = "http://vitibrasil.cnpuv.embrapa.br/download/ProcessaSemclass.csv"
+        dados_json = Processamento.baixar_e_processar_csv(url)
+        return dados_json, 200
+    
+class ProcessaAmericanas(Resource):
+    @api.doc('processa_americanas')
+    @api.response(200, 'Sucesso')
+    @jwt_required()
+    def get(self):
+        """Processa dados de americanas"""
+        url = "http://vitibrasil.cnpuv.embrapa.br/download/ProcessaAmericanas.csv"
+        dados_json = Processamento.baixar_e_processar_csv(url)
+        return dados_json, 200    
 
 # Adiciona as rotas ao namespace
 api.add_resource(ProcessaMesa, '/processamesa')
 api.add_resource(ProcessaVinifera, '/processavinifera')
-
-# A chamada para adicionar os recursos ao namespace deve ser removida daqui
+api.add_resource(ProcessaSemClass, '/processasemclass')
+api.add_resource(ProcessaAmericanas, '/processaamericanas')
